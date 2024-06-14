e3cf481ec8b9ea3c04d474044b04fbbf810ade50
Pourya-Ardestani <pooryaardestani@gmail.com>
2024-06-14 16:41:50 +0330

    add washMachine file and attributes

---
+++ WatchingMachine.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+class WatchingMatchine
+{
+private:
+	int washTime;
+	int rinseTime;
+	int spinTime;
+public:
+	void wash();
+	void rinse();
+	void spin();
+	void fill();
+	void empty();
+	void standardWash();
+	void twiceRinse();
+};
+
+int main()
+{
+
+}
